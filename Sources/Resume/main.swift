9fc8feefc51f3361f237a79dc184e6c363a01ad3
Tyler Thompson <Tyler.Keith.Thompson@gmail.com>
2022-01-03 23:32:01 -0700

    [resume] - Added about me section to resume - TT

--- Sources/Resume/main.swift
+++ Sources/Resume/main.swift
@@ -13,12 +13,25 @@ struct Resume: Website {
         // Add any site-specific metadata that you want to use here.
     }
 
+    struct Content {
+        let profileName = "Tyler Thompson"
+        let profileTitle = "Principal Software Engineer - Apple Platforms"
+        let aboutMe = """
+        My primary motivation is helping people. As Software Engineer's we can sometimes get so wrapped up in a problem we forget what the end goal is. I do my best to make sure the software I help write makes a difference, and is based off of user needs.
+
+        I have managed to become a Principal Engineer largely by working with people far smarter than I am, and learning from them. I owe much of my success specifically to World Wide Technology where I continue to enjoy opportunities to work with a very diverse group of intelligent people.
+
+        I spend most of my free time honing my skills in my favorite programming language (Swift). I've become an avid believer in automated testing and DevOps practices. I incorporate that into both my professional code and my personal code.
+        """
+    }
+
     // Update these properties to configure your website:
     var url = URL(string: "https://your-website-url.com")!
     var name = "Tyler Thompson's Resume"
     var description = "Tyler Thompson's online resume"
     var language: Language { .english }
     var imagePath: Path? { nil }
+    let content = Content()
 }
 
 // This will generate your website using the built-in Foundation theme:
